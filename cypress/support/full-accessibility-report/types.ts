acda79c83e76b26a0aa3065a8c50cf730247d416
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-04-17 17:22:31 +0200

    outsource custom types

---
+++ cypress/support/full-accessibility-report/types.ts
@@ -0,0 +1,30 @@
+type ImpactType = 'serious' | 'critical' | 'moderate'
+type TagType = 'wcag2a' | 'wcag2aa' | 'wcag21a' | 'wcag21aa' | 'wcag22aa'
+
+export type CustomViolationType = {
+    description: string
+    failureSummary: string[]
+    help: string
+    helpUrl: string
+    html: string
+    id: string
+    impact: ImpactType
+    tags: TagType[]
+}
+
+export type CustomViolationReturnType = {
+    id: string
+    impact: ImpactType
+    description: string
+    help: string
+    helpUrl: string
+    nodes: [
+        {
+            failureSummary: string
+            html: string
+            impact: ImpactType
+            target: [string]
+        },
+    ]
+    tags: TagType[]
+}
