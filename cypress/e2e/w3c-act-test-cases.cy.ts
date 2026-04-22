16171d0ad95e5cac438e295defb371011b37bd7f
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-04-22 14:35:56 +0200

    w3c act rules benchmark

---
+++ cypress/e2e/w3c-act-test-cases.cy.ts
@@ -0,0 +1,40 @@
+import { runAxeAudit } from '../support/full-accessibility-report/auditor'
+
+describe('System Benchmark: W3C ACT Rules Validation', () => {
+    const benchmarkData = require('../fixtures/testcases.json')
+
+    benchmarkData.testcases.forEach((tc: any) => {
+        it(`Benchmark ${tc.testcaseTitle}`, () => {
+            const currentErrors: string[] = []
+
+            cy.visit(tc.url)
+
+            runAxeAudit(tc.url, currentErrors)
+
+            cy.then(() => {
+                const foundAnyIssue = currentErrors.length > 0
+
+                if (tc.expected === 'failed') {
+                    if (foundAnyIssue) {
+                        cy.log('Issue found.')
+                    } else {
+                        throw new Error(
+                            `Error expected, but not found: ${tc.testcaseTitle}`
+                        )
+                    }
+                } else if (
+                    tc.expected === 'passed' ||
+                    tc.expected === 'inapplicable'
+                ) {
+                    if (!foundAnyIssue) {
+                        cy.log('No false positive triggered.')
+                    } else {
+                        throw new Error(
+                            `False positive: Framework reports errors on a correct page: ${currentErrors.join('\n')}`
+                        )
+                    }
+                }
+            })
+        })
+    })
+})
