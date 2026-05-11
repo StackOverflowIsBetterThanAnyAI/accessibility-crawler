e717b2cb5674a5833302fb064a22bc7e0ca7391d
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-05-11 16:48:23 +0200

    no-unused-vars

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     e2e: {
         allowCypressEnv: false,
         baseUrl: 'http://localhost:5173',
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         setupNodeEvents(on, _config) {
             on('task', {
                 checkIfFileExists(path: string) {
