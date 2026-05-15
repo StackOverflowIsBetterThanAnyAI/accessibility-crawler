5860a92ad3bcb42fb7407b5919652e85f7b26f3d
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-05-15 16:45:41 +0200

    wait for network idle

--- cypress/support/full-accessibility-audit/auditor.ts
+++ cypress/support/full-accessibility-audit/auditor.ts
@@ -3,14 +3,14 @@ import * as BodyChecks from './auditor-checks-body'
 import * as HeadChecks from './auditor-checks-head'
 import * as HtmlChecks from './auditor-checks-html'
 import { processViolations } from './auditor-helper'
-import { waitForStableDOM } from './wait-for-stable-dom'
+import { waitForNetworkIdle } from './wait-for-network-idle'
 import { CustomViolationReturnType } from './types'
 
 export const runAxeAudit = (
     currentPath: string,
     errorList: { id: string; message: string }[]
 ) => {
-    waitForStableDOM()
+    waitForNetworkIdle()
 
     cy.injectAxe()
 
