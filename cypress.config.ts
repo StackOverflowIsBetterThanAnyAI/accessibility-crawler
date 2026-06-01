eb319f2c2bbe7710d83b831cf82d61a49d885036
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-06-01 21:02:45 +0200

    screenshotOnRunFailure: false

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs'
 export default defineConfig({
     e2e: {
         allowCypressEnv: false,
+        screenshotOnRunFailure: false,
         baseUrl: 'http://localhost:5173',
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         setupNodeEvents(on, _config) {
